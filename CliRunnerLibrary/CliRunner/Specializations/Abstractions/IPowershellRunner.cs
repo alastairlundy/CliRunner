ccc3de464d3c1aebc560fe07cb3fdfc735283d8c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-09-30 02:08:54 +0100

    add Windows Powershell runner

---
+++ CliRunnerLibrary/CliRunner/Specializations/Abstractions/IPowershellRunner.cs
@@ -0,0 +1,37 @@
+﻿/*
+    CliRunner 
+    Copyright (C) 2024  Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   */
+
+using System;
+using System.Runtime.Versioning;
+using CliRunner.Processes.Abstractions;
+
+namespace CliRunner.Specializations.Abstractions
+{
+    public interface IPowershellRunner
+    {
+        #if NET5_0_OR_GREATER
+        [SupportedOSPlatform("windows")]
+        [SupportedOSPlatform("macos")]
+        [SupportedOSPlatform("linux")]
+        #endif
+        IProcessResult Execute(string commandLine, bool runAsAdministrator);
+
+        bool IsInstalled();
+        
+        Version GetInstalledVersion();
+    }
+}
