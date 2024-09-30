686a79da81ee8cb92c42e966defc37a8fbf7db8a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-09-30 14:51:21 +0100

    Create IExecutable.cs

---
+++ CliRunnerLibrary/CliRunner/Processes/Abstractions/IExecutable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CliRunner.Processes.Abstractions
+{
+    public interface IExecutable
+    {
+        string Name { get; }
+        
+        string FilePath { get; }
+        
+        IEnumerable<string> Arguments { get; }
+        
+        ProcessStartInfo? StartInfo { get; }
+        
+        
+    }
+}
