48602c721aedaae4ff9f1396966164a8fdea0e86
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-09-30 02:12:29 +0100

    add initial PowershellRunner and Executable runner code

---
+++ CliRunnerLibrary/CliRunner/Specializations/PowershellRunner.cs
@@ -0,0 +1,41 @@
+﻿/*
+    CliRunner 
+    Copyright (C) 2024  Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   */
+
+using System;
+using CliRunner.Processes.Abstractions;
+using CliRunner.Specializations.Abstractions;
+
+namespace CliRunner.Specializations
+{
+    public class PowershellRunner : IPowershellRunner
+    {
+        public IProcessResult Execute(string commandLine, bool runAsAdministrator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsInstalled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Version GetInstalledVersion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
