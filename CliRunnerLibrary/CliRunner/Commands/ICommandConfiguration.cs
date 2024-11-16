27975af0af72bd4b863eadeaf026a18e96f391b2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-11-16 20:43:31 +0000

    Create ICommandConfiguration.cs

---
+++ CliRunnerLibrary/CliRunner/Commands/ICommandConfiguration.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Based on Tyrrrz's CliWrap ICommandCongifuration.cs
+ * https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/ICommandConfiguration.cs
+
+Portions of this code are licensed under the MIT license.
+
+ */
+
+using System.Collections.Generic;
+using CliRunner.Piping.Abstractions;
+
+namespace CliRunner.Commands
+{
+    public interface ICommandConfiguration
+    {
+        public string TargetFilePath { get; }
+        public string WorkingDirectoryPath { get; }
+        
+        public string[] Arguments { get; }
+
+        public IReadOnlyDictionary<string, string> EnvironmentVariables { get; }
+        
+        public CliRunner.Credentials Credentials { get;  }
+        public CliRunner.Processes.ProcessResultValidator ProcessResultValidator { get; }
+         
+        public AbstractPipeSource StandardInputPipe { get; }
+        public AbstractPipeTarget StandardOutputPipe { get; }
+        public AbstractPipeTarget StandardErrorPipe { get; }
+    }
+}
