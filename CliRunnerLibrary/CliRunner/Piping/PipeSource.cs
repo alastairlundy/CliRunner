1626b0b2e1af7490a545d9d5b27e39393b0146e2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-08 20:11:16 +0000

    merge Abstract classes into Piping classes with static methods

---
+++ CliRunnerLibrary/CliRunner/Piping/PipeSource.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CliRunner.Commands;
+using CliRunner.Piping.Abstractions;
+
+namespace CliRunner.Piping
+{
+    public abstract class PipeSource
+    {
+        public abstract Task CopyToAsync(Stream destination, CancellationToken cancellationToken = default);
+        
+        public static PipeSource Null =>
+            Create((Stream _, CancellationToken cancellationToken) => cancellationToken.IsCancellationRequested
+                ? Task.FromCanceled(cancellationToken)
+                : Task.CompletedTask);
+
+        public static PipeSource Create(Func<Stream, CancellationToken, Task> handlePipeAsync)
+        {
+            
+        }
+
+        public static PipeSource Create(Action<Stream> handlePipe)
+        {
+            
+        }
+
+        public static PipeSource FromStream(Stream stream, bool autoFlush)
+        {
+            
+        }
+
+        public static PipeSource FromStream(Stream stream)
+        {
+            
+        }
+
+        public static PipeSource FromFile(string filePath)
+        {
+            
+        }
+
+        public static PipeSource FromBytes(ReadOnlyMemory<byte> data)
+        {
+            
+        }
+
+        public static PipeSource FromBytes(byte[] data)
+        {
+            
+        }
+
+        public static PipeSource FromString(string str, Encoding encoding)
+        {
+            
+        }
+
+        public static PipeSource FromString(string str)
+        {
+            
+        }
+
+        public static PipeSource FromCommand(Command command)
+        {
+            
+        }
+    }
+}
---
+++ CliRunnerLibrary/CliRunner/Piping/PipeTarget.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CliRunner.Piping.Abstractions;
+
+namespace CliRunner.Piping
+{
+    public abstract class PipeTarget
+    {
+        public abstract Task CopyFromAsync(Stream origin, CancellationToken cancellationToken = default);
+        
+        public static PipeTarget Null =>
+            Create((Stream _, CancellationToken cancellationToken) => cancellationToken.IsCancellationRequested
+                ? Task.FromCanceled(cancellationToken)
+                : Task.CompletedTask);
+        
+        
+        public static PipeTarget Create(Func<Stream, CancellationToken, Task> handlePipeAsync)
+        {
+            
+        }
+
+        public static PipeTarget Create(Action<Stream> handlePipe)
+        {
+            
+        }
+
+        public static PipeTarget ToStream(Stream stream, bool autoFlush)
+        {
+            
+        }
+
+        public static PipeTarget ToStream(Stream stream)
+        {
+            
+        }
+
+        public static PipeTarget ToFile(string filePath)
+        {
+            
+        }
+
+        public static PipeTarget ToStringBuilder(StringBuilder builder, Encoding encoding)
+        {
+            
+        }
+
+        public static PipeTarget ToStringBuilder(StringBuilder builder)
+        {
+            return ToStringBuilder(builder, Encoding.Default);
+        }
+
+        public static PipeTarget ToDelegate(Func<string, CancellationToken, Task> handleLineAsync, Encoding encoding)
+        {
+            
+        }
+        
+        public static PipeTarget ToDelegate(Func<string, CancellationToken, Task> handleLineAsync)
+        {
+            return ToDelegate(handleLineAsync, Encoding.Default);
+        }
+
+        public static PipeTarget ToDelegate(Action<string> handleLine, Encoding encoding)
+        {
+            
+        }
+        
+        public static PipeTarget ToDelegate(Action<string> handleLine)
+        {
+            return ToDelegate(handleLine, Encoding.Default);
+        }
+
+        public static PipeTarget Merge(IEnumerable<PipeTarget> targets)
+        {
+            
+        }
+
+        public static PipeTarget Merge(params PipeTarget[] targets)
+        {
+            
+        }
+    }
+}
