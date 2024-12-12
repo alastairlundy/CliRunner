2964b405808f5425e3e3c9f68612f6f651f1bf82
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-12 15:52:00 +0000

    update license notices as needed

--- CliRunnerLibrary/CliRunner/Commands/CommandResult.cs
+++ CliRunnerLibrary/CliRunner/Commands/CommandResult.cs
@@ -1,9 +1,16 @@
 ﻿/*
-   Based on Tyrrrz's CliWrap CommandResult.cs
-   https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/CommandResult.cs
+    CliRunner
+    Copyright (C) 2024  Alastair Lundy
 
-   Portions of this code are licensed under the MIT license.
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+    Based on Tyrrrz's CliWrap CommandResult.cs
+    https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/CommandResult.cs
+
+     Constructor signature and field declarations from CliWrap licensed under the MIT License except where considered Copyright Fair Use by law.
+     See THIRD_PARTY_NOTICES.txt for a full copy of the MIT LICENSE.
  */
 
 using System;
--- CliRunnerLibrary/CliRunner/Commands/CommandResultValidation.cs
+++ CliRunnerLibrary/CliRunner/Commands/CommandResultValidation.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Based on Tyrrrz's CliWrap CommandResultValidation.cs
+    https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/CommandResultValidation.cs
+
+     See THIRD_PARTY_NOTICES.txt for a full copy of the MIT LICENSE.
+ */
+
+using System;
 
 namespace CliRunner.Commands
 {
