310f48a6ab7f5b0a2e1c617bfd3c1b8d530a5e58
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-12 15:26:48 +0000

    cleanup and add to Command class

--- CliRunnerLibrary/CliRunner/Commands/Command.cs
+++ CliRunnerLibrary/CliRunner/Commands/Command.cs
@@ -1,11 +1,17 @@
 ﻿/*
-    CliRunner 
+    CliRunner
     Copyright (C) 2024  Alastair Lundy
 
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
-   */
+
+    Based on Tyrrrz's CliWrap Command.cs
+    https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/Command.cs
+
+     Constructor signature and field declarations from CliWrap licensed under the MIT License except where considered Copyright Fair Use by law.
+     See THIRD_PARTY_NOTICES.txt for a full copy of the MIT LICENSE.
+ */
 
 using System;
 using System.Collections.Generic;
@@ -24,13 +30,31 @@ namespace CliRunner.Commands
         public string Arguments { get; protected set; }
         
         public IReadOnlyDictionary<string, string> EnvironmentVariables { get; protected set; }
-        public CliRunner.Commands.CommandResultValidation CommandResultValidation { get; protected set;}
         public UserCredentials Credentials { get; protected set; }
+        public CommandResultValidation ResultValidation { get; protected set;}
         
         public StreamWriter StandardInput{ get; protected set; }
         public StreamReader StandardOutput { get; protected set; }
         public StreamReader StandardError { get; protected set; }
+        
+        public IntPtr ProcessorAffinity { get; protected set; }
+        
+        public bool UseShellExecute { get; protected set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetFilePath"></param>
+        /// <param name="arguments"></param>
+        /// <param name="workingDirectoryPath"></param>
+        /// <param name="runAsAdministrator"></param>
+        /// <param name="environmentVariables"></param>
+        /// <param name="credentials"></param>
+        /// <param name="commandResultValidation"></param>
+        /// <param name="standardInput"></param>
+        /// <param name="standardOutput"></param>
+        /// <param name="standardError"></param>
+        /// <param name="processorAffinity"></param>
         public Command(string targetFilePath,
              string arguments = null, string workingDirectoryPath = null,
              bool runAsAdministrator = false,
@@ -40,6 +64,8 @@ namespace CliRunner.Commands
              StreamWriter standardInput = null,
              StreamReader standardOutput = null,
              StreamReader standardError = null,
+             IntPtr processorAffinity = default(IntPtr),
+             bool useShellExecute = false
         )
         {
             TargetFilePath = targetFilePath;
@@ -49,13 +75,21 @@ namespace CliRunner.Commands
             EnvironmentVariables = environmentVariables ?? new Dictionary<string, string>();
             Credentials = credentials ?? UserCredentials.Default;
 
-            CommandResultValidation = commandResultValidation;
+            ResultValidation = commandResultValidation;
+            
             StandardInput = standardInput ?? StreamWriter.Null;
             StandardOutput = standardOutput ?? StreamReader.Null;
             StandardError = standardError ?? StreamReader.Null;
             
+            ProcessorAffinity = processorAffinity;
+            UseShellExecute = useShellExecute;
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
         public Command WithArguments(IEnumerable<string> arguments) =>
             new Command(TargetFilePath,
                 string.Join(" ", arguments),
@@ -63,11 +97,19 @@ namespace CliRunner.Commands
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <param name="escape"></param>
+        /// <returns></returns>
         public Command WithArguments(IEnumerable<string> arguments, bool escape)
         {
             return new Command(TargetFilePath,
@@ -76,12 +118,19 @@ namespace CliRunner.Commands
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
         public Command WithArguments(string arguments)=> 
             new Command(TargetFilePath,
                 arguments,
@@ -89,11 +138,18 @@ namespace CliRunner.Commands
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetFilePath"></param>
+        /// <returns></returns>
         public Command WithTargetFile(string targetFilePath) => 
             new Command(targetFilePath,
             Arguments,
@@ -101,12 +157,19 @@ namespace CliRunner.Commands
             RunAsAdministrator,
             EnvironmentVariables,
             Credentials,
-            CommandResultValidation,
+            ResultValidation,
             StandardInput,
             StandardOutput,
             StandardError,
+            ProcessorAffinity,
+            UseShellExecute);
         
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="environmentVariables"></param>
+        /// <returns></returns>
         public Command WithEnvironmentVariables(IReadOnlyDictionary<string, string> environmentVariables) =>
             new Command(TargetFilePath,
                 Arguments,
@@ -114,11 +177,18 @@ namespace CliRunner.Commands
                 RunAsAdministrator,
                 environmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configure"></param>
+        /// <returns></returns>
         public Command WithEnvironmentVariables(Action<EnvironmentVariablesBuilder> configure)
         {
             var environmentVariablesBuilder = new EnvironmentVariablesBuilder()
@@ -129,6 +199,11 @@ namespace CliRunner.Commands
             return WithEnvironmentVariables(environmentVariablesBuilder.Build());
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="runAsAdministrator"></param>
+        /// <returns></returns>
         public Command RequiresAdministrator(bool runAsAdministrator) =>
             new Command(TargetFilePath,
             Arguments,
@@ -136,11 +211,18 @@ namespace CliRunner.Commands
             runAsAdministrator,
             EnvironmentVariables,
             Credentials,
-            CommandResultValidation,
+            ResultValidation,
             StandardInput,
             StandardOutput,
             StandardError,
+            ProcessorAffinity,
+            UseShellExecute);
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="workingDirectoryPath"></param>
+        /// <returns></returns>
         public Command WithWorkingDirectory(string workingDirectoryPath) =>
             new Command(TargetFilePath,
                 Arguments,
@@ -148,34 +230,54 @@ namespace CliRunner.Commands
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
         
         
-        public Command WithCredentials(Credentials credentials) =>
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="credentials"></param>
+        /// <returns></returns>
+        public Command WithCredentials(UserCredentials credentials) =>
             new Command(TargetFilePath,
                 Arguments,
                 WorkingDirectoryPath,
                 RunAsAdministrator,
                 EnvironmentVariables,
                 credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configure"></param>
+        /// <returns></returns>
         public Command WithCredentials(Action<CredentialsBuilder> configure)
         {
             var credentialBuilder = new CredentialsBuilder().SetDomain(Credentials.Domain)
                 .SetPassword(Credentials.Password)
+                .SetUsername(Credentials.UserName);
 
            configure(credentialBuilder);
            
            return WithCredentials(credentialBuilder.Build());
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="validation"></param>
+        /// <returns></returns>
         public Command WithValidation(CommandResultValidation validation) =>
             new Command(TargetFilePath,
                 Arguments,
@@ -187,6 +289,8 @@ namespace CliRunner.Commands
                 StandardInput,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
         
         /// <summary>
         /// 
@@ -200,44 +304,87 @@ namespace CliRunner.Commands
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 source,
                 StandardOutput,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
         
         /// <summary>
         /// 
         /// </summary>
         /// <param name="target"></param>
         /// <returns></returns>
-        public Command WithStandardOutputStream(StreamReader target) =>
+        public Command WithStandardOutput(StreamReader target) =>
             new Command(TargetFilePath,
                 Arguments,
                 WorkingDirectoryPath,
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 target,
                 StandardError,
+                ProcessorAffinity,
+                UseShellExecute);
         
         /// <summary>
         /// 
         /// </summary>
         /// <param name="target"></param>
         /// <returns></returns>
-        public Command WithStandardErrorStream(StreamReader target) =>
+        public Command WithStandardError(StreamReader target) =>
             new Command(TargetFilePath,
                 Arguments,
                 WorkingDirectoryPath,
                 RunAsAdministrator,
                 EnvironmentVariables,
                 Credentials,
-                CommandResultValidation,
+                ResultValidation,
                 StandardInput,
                 StandardOutput,
                 target,
+                ProcessorAffinity,
+                UseShellExecute);
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="processorAffinity"></param>
+        /// <returns></returns>
+        public Command WithProcessorAffinity(IntPtr processorAffinity) =>
+            new Command(TargetFilePath,
+                Arguments,
+                WorkingDirectoryPath,
+                RunAsAdministrator,
+                EnvironmentVariables,
+                Credentials,
+                ResultValidation,
+                StandardInput,
+                StandardOutput,
+                StandardError,
+                processorAffinity,
+                UseShellExecute);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="useShellExecute"></param>
+        /// <returns></returns>
+        public Command WithShellExecute(bool useShellExecute) =>
+            new Command(TargetFilePath,
+                Arguments,
+                WorkingDirectoryPath,
+                RunAsAdministrator,
+                EnvironmentVariables,
+                Credentials,
+                ResultValidation,
+                StandardInput,
+                StandardOutput,
+                StandardError,
+                ProcessorAffinity,
+                useShellExecute);
     }
 }
