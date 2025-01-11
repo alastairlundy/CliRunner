5446dbefca7981cf5e30d1649b999537a4d15687
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-11 17:41:29 +0000

    replace Run extension method with static Create Command method

--- CliRunnerLibrary/CliRunner/Commands/Command.cs
+++ CliRunnerLibrary/CliRunner/Commands/Command.cs
@@ -33,7 +33,7 @@ using CliRunner.Internal.Localizations;
 namespace CliRunner.Commands
 {
     [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
-    public partial class Command : ICommand
+    public class Command : ICommand
     {
         /// <summary>
         /// Whether administrator privileges are required when executing the Command.
@@ -160,6 +160,21 @@ namespace CliRunner.Commands
             WindowCreation = windowCreation;
         }
 
+                
+        /// <summary>
+        /// Creates a Command object with the specified target file path. 
+        /// </summary>
+        /// <remarks>
+        /// <para>Chain appropriate Command methods as needed such as <code>.WithArguments("[your args]")</code> and <code>.WithWorkingDirectory("[your directory]")</code>.</para>
+        /// <para>Don't forget to call <code>.ExecuteAsync();</code> or <code>.ExecuteBufferedAsync();</code> when you're ready to execute the Command!</para>
+        /// </remarks>
+        /// <param name="targetFilePath">The target file path of the executable to wrap.</param>
+        /// <returns>A new Command object with the configured Target File Path.</returns>
+        public static Command Create(string targetFilePath)
+        {
+            return new Command(targetFilePath);
+        }
+        
         /// <summary>
         /// Sets the arguments to pass to the executable.
         /// </summary>
