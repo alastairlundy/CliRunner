a33ddd22eddc9bd1abc20654732643bca2f561d2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-12 13:00:16 +0000

    Update CommandRunner.cs

--- CliRunnerLibrary/CliRunner/Commands/CommandRunner.cs
+++ CliRunnerLibrary/CliRunner/Commands/CommandRunner.cs
@@ -31,6 +31,7 @@ using CliRunner.Exceptions;
 
 using CliRunner.Internal.Localizations;
 // ReSharper disable RedundantBoolCompare
+// ReSharper disable ConvertToPrimaryConstructor
 
 #if NETSTANDARD2_0 || NETSTANDARD2_1
 using OperatingSystem = AlastairLundy.Extensions.Runtime.OperatingSystemExtensions;
