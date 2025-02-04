240a23fa04ebe41f996a97aa4b65851dd5d7cae7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-04 18:21:51 +0000

    Update ICommandConfiguration.cs

--- CliRunnerLibrary/CliRunner/Abstractions/ICommandConfiguration.cs
+++ CliRunnerLibrary/CliRunner/Abstractions/ICommandConfiguration.cs
@@ -49,32 +49,91 @@ namespace CliRunner.Abstractions;
 /// </summary>
 public interface ICommandConfiguration
 {
-    bool RequiresAdministrator { get; }
-    string TargetFilePath { get; }
-    string WorkingDirectoryPath { get; }
+        /// <summary>
+        /// Whether administrator privileges are required when executing the Command.
+        /// </summary>
+        public bool RequiresAdministrator { get; }
 
-    string Arguments { get; }
+        /// <summary>
+        /// The file path of the executable to be run and wrapped.
+        /// </summary>
+        public string TargetFilePath { get; }
 
-    IReadOnlyDictionary<string, string> EnvironmentVariables { get; }
+        /// <summary>
+        /// The working directory path to be used when executing the Command.
+        /// </summary>
+        public string WorkingDirectoryPath { get; }
 
-    CommandResultValidation ResultValidation { get;}
+        /// <summary>
+        /// The arguments to be provided to the executable to be run.
+        /// </summary>
+        public string Arguments { get; }
 
-    StreamWriter StandardInput { get; }
-    StreamReader StandardOutput { get; }
-    StreamReader StandardError { get; }
+        /// <summary>
+        /// Whether to enable window creation or not when the Command's Process is run.
+        /// </summary>
+        public bool WindowCreation { get; }
+
+        /// <summary>
+        /// The environment variables to be set.
+        /// </summary>
+        public IReadOnlyDictionary<string, string> EnvironmentVariables { get; }
+
+        /// <summary>
+        /// The credentials to be used when executing the executable.
+        /// </summary>
         public UserCredential Credentials { get; }
-        
-    bool UseShellExecution { get; }
 
-    bool WindowCreation { get; }
-        
-    Encoding StandardInputEncoding { get; }
-    Encoding StandardOutputEncoding { get; }
-    Encoding StandardErrorEncoding { get; }
-        
+        /// <summary>
+        /// The result validation to apply to the Command when it is executed.
+        /// </summary>
+        public CommandResultValidation ResultValidation { get; }
+
+        /// <summary>
+        /// The Standard Input source.
+        /// </summary>
+        /// <remarks>Using Shell Execution whilst also Redirecting Standard Input will throw an Exception. This is a known issue with the System Process class.</remarks>
+        /// <seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo.redirectstandarderror" />
+        public StreamWriter StandardInput { get; }
+
+        /// <summary>
+        /// The Standard Output target.
+        /// </summary>
+        public StreamReader StandardOutput { get; }
+
+        /// <summary>
+        /// The Standard Error target.
+        /// </summary>
+        public StreamReader StandardError { get; }
+
+        /// <summary>
+        /// The processor threads to be used for executing the Command.
+        /// </summary>
 #if NET6_0_OR_GREATER
-    [SupportedOSPlatform("windows")]
-    [SupportedOSPlatform("linux")]
+        [SupportedOSPlatform("windows")]
+        [SupportedOSPlatform("linux")]
 #endif
-    IntPtr ProcessorAffinity { get; }
+        public IntPtr ProcessorAffinity { get; }
+
+        /// <summary>
+        /// Whether to use Shell Execution or not.
+        /// </summary>
+        /// <remarks>Using Shell Execution whilst also Redirecting Standard Input will throw an Exception. This is a known issue with the System Process class.</remarks>
+        /// <seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo.redirectstandarderror" />
+        public bool UseShellExecution { get; }
+        
+        /// <summary>
+        /// The encoding to use for the Standard Input.
+        /// </summary>
+        public Encoding StandardInputEncoding { get; }
+        
+        /// <summary>
+        /// The encoding to use for the Standard Output.
+        /// </summary>
+        public Encoding StandardOutputEncoding { get; }
+        
+        /// <summary>
+        /// The encoding to use for the Standard Error.
+        /// </summary>
+        public Encoding StandardErrorEncoding { get;  }
 }
