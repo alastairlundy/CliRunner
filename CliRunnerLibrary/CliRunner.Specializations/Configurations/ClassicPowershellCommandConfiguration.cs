8f92dace19007752413be8bc419a2cac4f2241cf
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-05 21:30:05 +0000

    Update ClassicPowershellCommandConfiguration.cs

--- CliRunnerLibrary/CliRunner.Specializations/Configurations/ClassicPowershellCommandConfiguration.cs
+++ CliRunnerLibrary/CliRunner.Specializations/Configurations/ClassicPowershellCommandConfiguration.cs
@@ -11,7 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-
+ 
 using CliRunner.Extensibility;
 using CliRunner.Specializations.Internal.Localizations;
 
