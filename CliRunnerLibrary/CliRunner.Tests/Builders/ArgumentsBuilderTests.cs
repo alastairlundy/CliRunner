53a30ace7fd3805518e882b7efdc44386d36c7d8
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-05 22:38:38 +0000

    Update ArgumentsBuilderTests.cs

--- CliRunnerLibrary/CliRunner.Tests/Builders/ArgumentsBuilderTests.cs
+++ CliRunnerLibrary/CliRunner.Tests/Builders/ArgumentsBuilderTests.cs
@@ -1,7 +1,17 @@
-﻿namespace CliRunner.Tests.Builders;
+﻿using CliRunner.Builders;
+
+namespace CliRunner.Tests.Builders;
 
 public class ArgumentsBuilderTests
 {
+    [Fact]
+    public void BuilderChainingTest()
+    {
+        ArgumentsBuilder argumentsBuilder = new ArgumentsBuilder()
+            .Add("-v");
+
+        Assert.True(string.IsNullOrEmpty(argumentsBuilder.Build()));
+    }
     
      
 }
