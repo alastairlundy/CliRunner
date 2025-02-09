f0b33d1b6ae0d6bb86f6a13accd35adcd0dbf969
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-09 16:03:54 +0000

    update to use xUnit 3

--- CliRunnerLibrary/CliRunner.Tests/Builders/CommandBuilderTests.cs
+++ CliRunnerLibrary/CliRunner.Tests/Builders/CommandBuilderTests.cs
@@ -1,10 +1,26 @@
-﻿namespace CliRunner.Tests.Builders;
+﻿using System.Windows.Input;
+using CliRunner.Builders;
+using CliRunner.Builders.Abstractions;
+
+namespace CliRunner.Tests.Builders;
 
 public class CommandBuilderTests
 {
+
+    [Fact]
+    public void TestDefaultConfiguration()
+    {
+        ICommandBuilder commandBuilder = new CommandBuilder("foo");
+        
+        
+        
+        
+    }
 
     public void TestIncompatiblePipingOptions()
     {
         
     }
+    
+    
 }
