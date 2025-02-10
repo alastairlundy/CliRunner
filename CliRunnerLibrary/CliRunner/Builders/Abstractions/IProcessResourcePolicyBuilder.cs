497993fb0b2d5f3117329b1fdfa518765881c283
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-10 12:15:05 +0000

    create ProcessResourcePolicy builder and interface

---
+++ CliRunnerLibrary/CliRunner/Builders/Abstractions/IProcessResourcePolicyBuilder.cs
@@ -0,0 +1,25 @@
+﻿/*
+    CliRunner 
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using System.Diagnostics;
+
+namespace CliRunner.Builders.Abstractions;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IProcessResourcePolicyBuilder
+{
+    IProcessResourcePolicyBuilder WithProcessorAffinity(nint processorAffinity);
+    IProcessResourcePolicyBuilder WithMinWorkingSet(nint minWorkingSet);
+    IProcessResourcePolicyBuilder WithMaxWorkingSet(nint maxWorkingSet);
+    IProcessResourcePolicyBuilder WithPriorityClass(ProcessPriorityClass processPriorityClass);
+    IProcessResourcePolicyBuilder WithPriorityBoost(bool enablePriorityBoost);
+    ProcessResourcePolicy Build();
+}
---
+++ CliRunnerLibrary/CliRunner/Builders/ProcessResourcePolicyBuilder.cs
@@ -0,0 +1,113 @@
+﻿/*
+    CliRunner 
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using System.Diagnostics;
+
+using CliRunner.Builders.Abstractions;
+
+namespace CliRunner.Builders;
+
+/// <summary>
+/// 
+/// </summary>
+public class ProcessResourcePolicyBuilder : IProcessResourcePolicyBuilder
+{
+    private readonly ProcessResourcePolicy _processResourcePolicy;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public ProcessResourcePolicyBuilder()
+    {
+        _processResourcePolicy = new ProcessResourcePolicy();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="processResourcePolicy"></param>
+    private ProcessResourcePolicyBuilder(ProcessResourcePolicy processResourcePolicy)
+    {
+        _processResourcePolicy = processResourcePolicy;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="processorAffinity"></param>
+    /// <returns></returns>
+    public IProcessResourcePolicyBuilder WithProcessorAffinity(nint processorAffinity) =>
+        new ProcessResourcePolicyBuilder(new ProcessResourcePolicy(
+            processorAffinity,
+            _processResourcePolicy.MinWorkingSet,
+            _processResourcePolicy.MaxWorkingSet,
+            _processResourcePolicy.PriorityClass,
+            _processResourcePolicy.EnablePriorityBoost));
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="minWorkingSet"></param>
+    /// <returns></returns>
+    public IProcessResourcePolicyBuilder WithMinWorkingSet(nint minWorkingSet) =>
+        new ProcessResourcePolicyBuilder(new ProcessResourcePolicy(
+            _processResourcePolicy.ProcessorAffinity,
+            minWorkingSet,
+            _processResourcePolicy.MaxWorkingSet,
+            _processResourcePolicy.PriorityClass,
+            _processResourcePolicy.EnablePriorityBoost));
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="maxWorkingSet"></param>
+    /// <returns></returns>
+    public IProcessResourcePolicyBuilder WithMaxWorkingSet(nint maxWorkingSet) =>
+        new ProcessResourcePolicyBuilder(new ProcessResourcePolicy(
+            _processResourcePolicy.ProcessorAffinity,
+            _processResourcePolicy.MinWorkingSet,
+            maxWorkingSet,
+            _processResourcePolicy.PriorityClass,
+            _processResourcePolicy.EnablePriorityBoost));
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="processPriorityClass"></param>
+    /// <returns></returns>
+    public IProcessResourcePolicyBuilder WithPriorityClass(ProcessPriorityClass processPriorityClass) =>
+        new ProcessResourcePolicyBuilder(new ProcessResourcePolicy(
+            _processResourcePolicy.ProcessorAffinity,
+            _processResourcePolicy.MinWorkingSet,
+            _processResourcePolicy.MaxWorkingSet,
+            processPriorityClass,
+            _processResourcePolicy.EnablePriorityBoost));
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="enablePriorityBoost"></param>
+    /// <returns></returns>
+    public IProcessResourcePolicyBuilder WithPriorityBoost(bool enablePriorityBoost) =>
+        new ProcessResourcePolicyBuilder(new ProcessResourcePolicy(
+            _processResourcePolicy.ProcessorAffinity,
+            _processResourcePolicy.MinWorkingSet,
+            _processResourcePolicy.MaxWorkingSet,
+            _processResourcePolicy.PriorityClass,
+            enablePriorityBoost));
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public ProcessResourcePolicy Build()
+    {
+        return _processResourcePolicy;
+    }
+}
