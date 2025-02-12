a7e8c17444c45788d905de95bc9e3c389852ce78
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-12 18:48:48 +0000

    add process piping code

---
+++ CliRunnerLibrary/CliRunner/Piping/ProcessPipeHandler.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+#if NET5_0_OR_GREATER
+using System.Runtime.Versioning;
+#endif
+
+using CliRunner.Piping.Abstractions;
+
+namespace CliRunner.Piping;
+
+/// <summary>
+/// 
+/// </summary>
+public class ProcessPipeHandler : IProcessPipeHandler
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+#if NET5_0_OR_GREATER
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("freebsd")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("maccatalyst")]
+    [UnsupportedOSPlatform("ios")]
+    [SupportedOSPlatform("android")]
+    [UnsupportedOSPlatform("tvos")]
+    [UnsupportedOSPlatform("browser")]
+#endif 
+    public async Task PipeStandardInputAsync(Process source, Process destination)
+    {
+        await destination.StandardInput.FlushAsync();
+        destination.StandardInput.BaseStream.Position = 0;
+        await source.StandardInput.BaseStream.CopyToAsync(destination.StandardInput.BaseStream);  
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+#if NET5_0_OR_GREATER
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("freebsd")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("maccatalyst")]
+    [UnsupportedOSPlatform("ios")]
+    [SupportedOSPlatform("android")]
+    [UnsupportedOSPlatform("tvos")]
+    [UnsupportedOSPlatform("browser")]
+#endif 
+    public async Task PipeStandardOutputAsync(Process source, Process destination)
+    {
+        destination.StandardOutput.DiscardBufferedData();
+        destination.StandardOutput.BaseStream.Position = 0;
+        await source.StandardOutput.BaseStream.CopyToAsync(destination.StandardOutput.BaseStream);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+#if NET5_0_OR_GREATER
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("freebsd")]
+    [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("maccatalyst")]
+    [UnsupportedOSPlatform("ios")]
+    [SupportedOSPlatform("android")]
+    [UnsupportedOSPlatform("tvos")]
+    [UnsupportedOSPlatform("browser")]
+#endif 
+    public async Task PipeStandardErrorAsync(Process source, Process destination)
+    {
+        destination.StandardError.DiscardBufferedData();
+        destination.StandardError.BaseStream.Position = 0;
+        await source.StandardError.BaseStream.CopyToAsync(destination.StandardError.BaseStream);
+    }
+}
