b4870b88598381b1486bc2d143597e8b817f4b05
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-13 17:41:29 +0000

    create ProcessRunnerUtility interface and class

---
+++ CliRunnerLibrary/CliRunner/Runners/Helpers/ProcessRunnerUtility.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using CliRunner.Runners.Helpers.Abstractions;
+
+namespace CliRunner.Runners.Helpers;
+
+public class ProcessRunnerUtility : IProcessRunnerUtility
+{
+    
+    public async Task ExecuteAsync(Process process, CancellationToken cancellationToken = default)
+    {
+        if (process.HasExited == false)
+        {
+            process.Start();
+            
+            await process.WaitForExitAsync(cancellationToken);
+        }
+        else
+        {
+            throw new InvalidOperationException("Cannot start a process that has exited.");
+        }
+    }
+    
+    /// <summary>
+    /// Disposes of the Process if it is running 
+    /// </summary>
+    /// <param name="process"></param>
+    public void DisposeOfProcess(Process process)
+    {
+        if (process.HasExited == false)
+        {
+            process.Kill();
+        }
+        
+        process.Close();
+        process.Dispose();
+    }
+
+    
+    public ProcessResult GetResult(Process process, bool disposeOfProcess)
+    {
+        if (process.HasExited == false)
+        {
+            if (Process.GetProcesses().Any(x => x.Equals(process)) == false)
+            {
+                 process.Start();
+            }
+            
+            process.WaitForExit();
+        }
+        
+        ProcessResult processResult = new ProcessResult(process.ExitCode, process.StartTime, process.ExitTime);
+
+        if (disposeOfProcess)
+        {
+            DisposeOfProcess(process);
+        }
+        
+        return processResult;
+    }
+
+    public BufferedProcessResult GetBufferedResult(Process process, bool disposeOfProcess)
+    {
+        if (process.HasExited == false)
+        {
+            if (Process.GetProcesses().Any(x => x.Equals(process)) == false)
+            {
+                process.Start();
+            }
+            
+            process.WaitForExit();
+        }
+        
+        BufferedProcessResult processResult = new BufferedProcessResult(process.ExitCode,
+             process.StandardOutput.ReadToEnd(),  process.StandardError.ReadToEnd(),
+            process.StartTime, process.ExitTime);
+
+        if (disposeOfProcess)
+        {
+            DisposeOfProcess(process);
+        }
+        
+        return processResult;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="process"></param>
+    /// <param name="disposeOfProcess"></param>
+    /// <returns></returns>
+    public async Task<ProcessResult> GetResultAsync(Process process, bool disposeOfProcess)
+    {
+        if (process.HasExited == false)
+        {
+            if (Process.GetProcesses().Any(x => x.Equals(process)) == false)
+            {
+               await ExecuteAsync(process);
+            }
+            else
+            {
+                await process.WaitForExitAsync();
+            }
+        }
+        
+        ProcessResult processResult = new ProcessResult(process.ExitCode, process.StartTime, process.ExitTime);
+
+        if (disposeOfProcess)
+        {
+            DisposeOfProcess(process);
+        }
+        
+        return processResult;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="process"></param>
+    /// <param name="disposeOfProcess">Whether to dispose of the process before returning.</param>
+    /// <returns>The BufferedProcessResult </returns>
+    public async Task<BufferedProcessResult> GetBufferedResultAsync(Process process, bool disposeOfProcess)
+    {
+        if (process.HasExited == false)
+        {
+            if (Process.GetProcesses().Contains(process) == false)
+            {
+                process.Start();
+            }
+            
+            await process.WaitForExitAsync();
+        }
+        
+        BufferedProcessResult processResult = new BufferedProcessResult(process.ExitCode,
+            await process.StandardOutput.ReadToEndAsync(), await process.StandardError.ReadToEndAsync(),
+            process.StartTime, process.ExitTime);
+
+        if (disposeOfProcess)
+        {
+            DisposeOfProcess(process);
+        }
+        
+        return processResult;
+    }
+}
