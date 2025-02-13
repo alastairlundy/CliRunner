d43ae1a2834857b4993056059ee992931e7b77f2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-13 13:54:09 +0000

    create IProcessRunner

---
+++ CliRunnerLibrary/CliRunner/Abstractions/IProcessRunner.cs
@@ -0,0 +1,57 @@
+﻿/*
+    CliRunner 
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CliRunner.Abstractions;
+
+/// <summary>
+/// An interface to specify safe Process Running functionality.
+/// </summary>
+public interface IProcessRunner
+{
+    /// <summary>
+    /// Runs the process synchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <remarks>Use the Async version of this method where possible to avoid UI freezes and other potential issues.</remarks>
+    /// <param name="process">The process to be run.</param>
+    /// <param name="processResultValidation">The process result validation to be used.</param>
+    /// <returns>The Process Results from the running the process.</returns>
+    public ProcessResult RunProcess(Process process, ProcessResultValidation processResultValidation);
+
+    /// <summary>
+    /// Runs the process synchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <remarks>Use the Async version of this method where possible to avoid UI freezes and other potential issues.</remarks>
+    /// <param name="process">The process to be run.</param>
+    /// <param name="processResultValidation">The process result validation to be used.</param>
+    /// <returns>The Buffered Process Results from running the process.</returns>
+    public BufferedProcessResult RunBufferedProcess(Process process, ProcessResultValidation processResultValidation);
+
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="process">The process to be run.</param>
+    /// <param name="processResultValidation">The process result validation to be used.</param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Process Results from the running the process.</returns>
+    public Task<ProcessResult> RunProcessAsync(Process process, ProcessResultValidation processResultValidation, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+    /// </summary>
+    /// <param name="process">The process to be run.</param>
+    /// <param name="processResultValidation">The process result validation to be used.</param>
+    /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+    /// <returns>The Buffered Process Results from running the process.</returns>
+    public Task<BufferedProcessResult> RunBufferedProcessAsync(Process process, ProcessResultValidation processResultValidation, CancellationToken cancellationToken = default);
+
+}
