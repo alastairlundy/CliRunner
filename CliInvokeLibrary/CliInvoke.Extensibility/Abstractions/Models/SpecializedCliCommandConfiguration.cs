6aefe18b3154e2cb9208d926e16c6e1ea63d3587
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-01 15:34:03 +0000

    Update SpecializedCliCommandConfiguration.cs

--- CliInvokeLibrary/CliInvoke.Extensibility/Abstractions/Models/SpecializedCliCommandConfiguration.cs
+++ CliInvokeLibrary/CliInvoke.Extensibility/Abstractions/Models/SpecializedCliCommandConfiguration.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
+
 using AlastairLundy.Extensions.Processes;
 
 // ReSharper disable UnusedType.Global
